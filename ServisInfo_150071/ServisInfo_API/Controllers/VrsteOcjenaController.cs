1d373d576f5a365f704877e38697debd5b1e077c
Ibrahim Šuta <suta.ibrahim@edu.fit.ba>
2018-05-16 17:14:10 +0200

    pocetni

---
+++ ServisInfo_150071/ServisInfo_API/Controllers/VrsteOcjenaController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ServisInfo_API.Models;
+
+namespace ServisInfo_API.Controllers
+{
+    public class VrsteOcjenaController : ApiController
+    {
+        private ServisInfoEntities db = new ServisInfoEntities();
+
+        // GET: api/VrsteOcjena
+        public IQueryable<VrsteOcjena> GetVrsteOcjena()
+        {
+            return db.VrsteOcjena;
+        }
+
+        // GET: api/VrsteOcjena/5
+        [ResponseType(typeof(VrsteOcjena))]
+        public IHttpActionResult GetVrsteOcjena(int id)
+        {
+            VrsteOcjena vrsteOcjena = db.VrsteOcjena.Find(id);
+            if (vrsteOcjena == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(vrsteOcjena);
+        }
+
+        // PUT: api/VrsteOcjena/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutVrsteOcjena(int id, VrsteOcjena vrsteOcjena)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != vrsteOcjena.VrstaOcjeneID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(vrsteOcjena).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VrsteOcjenaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/VrsteOcjena
+        [ResponseType(typeof(VrsteOcjena))]
+        public IHttpActionResult PostVrsteOcjena(VrsteOcjena vrsteOcjena)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.VrsteOcjena.Add(vrsteOcjena);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = vrsteOcjena.VrstaOcjeneID }, vrsteOcjena);
+        }
+
+        // DELETE: api/VrsteOcjena/5
+        [ResponseType(typeof(VrsteOcjena))]
+        public IHttpActionResult DeleteVrsteOcjena(int id)
+        {
+            VrsteOcjena vrsteOcjena = db.VrsteOcjena.Find(id);
+            if (vrsteOcjena == null)
+            {
+                return NotFound();
+            }
+
+            db.VrsteOcjena.Remove(vrsteOcjena);
+            db.SaveChanges();
+
+            return Ok(vrsteOcjena);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool VrsteOcjenaExists(int id)
+        {
+            return db.VrsteOcjena.Count(e => e.VrstaOcjeneID == id) > 0;
+        }
+    }
+}
---
+++ ServisInfo_150071/ServisInfo_API/Models/Model.Designer.cs
@@ -0,0 +1,10 @@
+﻿// T4 code generation is enabled for model 'C:\FIT_VI_semestar\RS 2\RSII-SeminarskiRad-150071\Aplikacija-150071\ServisInfo_150071\ServisInfo_API\Models\Model.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
+
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
