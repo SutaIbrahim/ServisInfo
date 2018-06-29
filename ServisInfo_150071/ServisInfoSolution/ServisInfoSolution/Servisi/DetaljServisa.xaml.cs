620b3d100ed46962c3988ac989c297d42555b36b
Ibrahim Šuta <suta.ibrahim@edu.fit.ba>
2018-06-29 17:32:04 +0200

    ocjene layout

--- ServisInfo_150071/ServisInfoSolution/ServisInfoSolution/Servisi/DetaljServisa.xaml.cs
+++ ServisInfo_150071/ServisInfoSolution/ServisInfoSolution/Servisi/DetaljServisa.xaml.cs
@@ -13,18 +13,18 @@ using Xamarin.Forms.Xaml;
 
 namespace ServisInfoSolution
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class DetaljServisa : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetaljServisa : ContentPage
+    {
         private WebAPIHelper servisiService = new WebAPIHelper("http://localhost:64158/", "api/Servisi");
 
         private int servisID;
 
-        public DetaljServisa (int servisID)
-		{
+        public DetaljServisa(int servisID)
+        {
             this.servisID = servisID;
-			InitializeComponent ();
-		}
+            InitializeComponent();
+        }
 
         protected override void OnAppearing()
         {
@@ -46,11 +46,32 @@ namespace ServisInfoSolution
                 TrajanjeLbl.Text = ponuda.TrajanjeDani.ToString();
                 CijenaLbl.Text = ponuda.Završna_cijena.ToString();
                 KompanijaLbl.Text = ponuda.Naziv_Kompanije;
+
+                if (ponuda.DatumZavršetka == null)
+                {
+                    porukaLbl.Text = "Servis je jos uvijek u toku";
+                    BrzinaSlider.IsVisible = false;
+                    KvalitetSlider.IsVisible = false;
+                    KomunikacijaSlider.IsVisible = false;
+                    
+                }
+                else
+                {
+                    porukaLbl.Text = "Servis je zavrsen, molimo vas da ocijenite uslugu";
+
+                    BrzinaSlider.IsVisible = true;
+                    KvalitetSlider.IsVisible = true;
+                    KomunikacijaSlider.IsVisible = true;
+                }
+
             }
 
 
-            }
+        }
 
+        private void ocjeniBtn_Clicked(object sender, EventArgs e)
+        {
 
+        }
     }
 }
