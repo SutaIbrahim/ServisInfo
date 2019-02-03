25f23a638d1b41f61d4caaa58339c6f624b6a184
Ibrahim Šuta <suta.ibrahim@edu.fit.ba>
2019-02-03 11:18:13 +0100

    rjesenja v3

--- ServisInfo_150071/ServisInfoSolution/ServisInfoSolution/Rjesenja/Rjesenja.xaml.cs
+++ ServisInfo_150071/ServisInfoSolution/ServisInfoSolution/Rjesenja/Rjesenja.xaml.cs
@@ -13,14 +13,14 @@ using Xamarin.Forms.Xaml;
 
 namespace ServisInfoSolution.Rjesenja
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Rjesenja : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Rjesenja : ContentPage
+    {
         private WebAPIHelper kategorijeService = new WebAPIHelper(Global.APIAdress, "api/Kategorije");
 
         int kId = 0;
-        public Rjesenja ()
-		{
+        public Rjesenja()
+        {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, true);
             Fill();
@@ -29,7 +29,7 @@ namespace ServisInfoSolution.Rjesenja
         public void Fill()
         {
             hideAll();
-       
+
 
             HttpResponseMessage response = kategorijeService.GetResponse();
 
@@ -66,6 +66,7 @@ namespace ServisInfoSolution.Rjesenja
             moguca.IsVisible = false;
 
             link.IsVisible = false;
+            nemaLbl.IsVisible = false;
 
             izaberiteLbl.IsVisible = true;
         }
@@ -76,7 +77,7 @@ namespace ServisInfoSolution.Rjesenja
 
             Kategorije k = (kategorijePicker.SelectedItem as Kategorije);
 
-            if (k!= null)
+            if (k != null)
             {
                 kId = k.KategorijaID;
 
@@ -84,8 +85,7 @@ namespace ServisInfoSolution.Rjesenja
 
                 pronadjiBtn.IsVisible = true;
 
-             
-                if (k.KategorijaID == 2)
+                if (k.KategorijaID == 2) //baterija
                 {
                     pronadjiBtn.IsVisible = true;
                     editor.IsVisible = true;
@@ -110,14 +110,75 @@ namespace ServisInfoSolution.Rjesenja
                     petiTxt.IsVisible = true;
                     petiTxt.Text = "Ne reaguje na punjenje";
                 }
+                else if (k.KategorijaID == 10) //punjenje
+                {
+
+                    pronadjiBtn.IsVisible = true;
+                    editor.IsVisible = true;
+
+                    prvi.IsVisible = true;
+                    prviTxt.IsVisible = true;
+                    prviTxt.Text = "Uređaj ne reaguje na punjač";
+
+                    drugi.IsVisible = true;
+                    drugiTxt.IsVisible = true;
+                    drugiTxt.Text = "Konektor punjenja je klimav";
+
+                    treci.IsVisible = true;
+                    treciTxt.IsVisible = true;
+                    treciTxt.Text = "Uređaj se puni ali vrlo sporo ";
+
+                    cetvrti.IsVisible = true;
+                    cetvrtiTxt.IsVisible = true;
+                    cetvrtiTxt.Text = "Punjenje se prekida";
+
+                    peti.IsVisible = true;
+                    petiTxt.IsVisible = true;
+                    petiTxt.Text = "Uređaj prepoznaje punjač ali ne puni mobitel";
+
+                }
+                else if (k.KategorijaID == 11) //displey
+                {
+
+                    pronadjiBtn.IsVisible = true;
+                    editor.IsVisible = true;
+
+                    prvi.IsVisible = true;
+                    prviTxt.IsVisible = true;
+                    prviTxt.Text = "Ekran uopšte ne daje sliku";
+
+                    drugi.IsVisible = true;
+                    drugiTxt.IsVisible = true;
+                    drugiTxt.Text = "Bijela svjetlost bez slike";
+
+                    treci.IsVisible = true;
+                    treciTxt.IsVisible = true;
+                    treciTxt.Text = "Linije raznih boja po ekranu ";
+
+                    cetvrti.IsVisible = true;
+                    cetvrtiTxt.IsVisible = true;
+                    cetvrtiTxt.Text = "Samo dio slike se prikazuje";
+
+                    peti.IsVisible = true;
+                    petiTxt.IsVisible = true;
+                    petiTxt.Text = "Slika se prikazuje ali touch ne radi";
+
+                }
+                else
+                {
+                    nemaLbl.IsVisible = true;
+                    pronadjiBtn.IsVisible = false;
+                }
+
 
 
             }
             else
             {
                 kId = 0;
+                izaberiteLbl.IsVisible = true;
             }
-            
+
         }
 
         private void pronadjiBtn_Clicked(object sender, EventArgs e)
@@ -127,21 +188,43 @@ namespace ServisInfoSolution.Rjesenja
                 editor.IsVisible = true;
                 moguca.IsVisible = true;
 
-                if (kId == 2)
+                if (kId == 2) //baterija
                 {
-                    if(peti.IsToggled || drugi.IsToggled)
+                    if (drugi.IsToggled)
                     {
                         editor.Text = " - Nažalost bateriju je potrebno zamijeniti";
                     }
-                    else if(prvi.IsToggled || cetvrti.IsToggled || treci.IsToggled)
+                    else if (peti.IsToggled && drugi.IsToggled)
                     {
+                        editor.Text = " - Nažalost bateriju je potrebno zamijeniti";
+                    }
+                    else if (cetvrti.IsToggled && peti.IsToggled)
+                    {
+                        editor.Text = " - Moguć problem u konektoru punjenja, pokušaje očistiti prašinu\n" +
+                            " - Provjerite adapter za punjenje, uvjerite se u njegovu ispravtnost \n" +
+                            " - Provjerite USB kabal\n" +
+                            " - Bateriju je potrebno kalibrirati (ispraznite bateriju na 0% te je napunite do kraja, u tom procesu uređaj nemojte koristiti)";
 
                         var tapGestureRecognizer = new TapGestureRecognizer();
-                        tapGestureRecognizer.Tapped += (s, ea) => {
+                        tapGestureRecognizer.Tapped += (s, ea) =>
+                        {
                             Device.OpenUri(new Uri("https://balkanandroid.com/kako-kalibrirati-podesiti-bateriju-na-vasem-androidu"));
                         };
 
-                        editor.Text = " - Bateriju je potrebno kalibrirati (ispraznite bateriju na 0% te je napunite do kraja)";
+                        link.IsVisible = true;
+                        link.GestureRecognizers.Add(tapGestureRecognizer);
+                        link.Text = " - Kliknite ovde u slučaju neuspjeha za napredniju kalibraciju";
+                    }
+                    else if (prvi.IsToggled || cetvrti.IsToggled || treci.IsToggled)
+                    {
+
+                        var tapGestureRecognizer = new TapGestureRecognizer();
+                        tapGestureRecognizer.Tapped += (s, ea) =>
+                        {
+                            Device.OpenUri(new Uri("https://balkanandroid.com/kako-kalibrirati-podesiti-bateriju-na-vasem-androidu"));
+                        };
+
+                        editor.Text = " - Bateriju je potrebno kalibrirati (ispraznite bateriju na 0% te je napunite do kraja, u tom procesu uređaj nemojte koristiti)";
 
                         link.IsVisible = true;
                         link.GestureRecognizers.Add(tapGestureRecognizer);
@@ -151,7 +234,116 @@ namespace ServisInfoSolution.Rjesenja
 
 
                 }
+                else if (kId == 10) //punjenje
+                {
+                    if (drugi.IsToggled)
+                    {
+                        editor.Text = " - Konektor je potrebno zamijenuti, javite se nekom od naših servisa";
+                    }
+                   else if (prvi.IsToggled)
+                    {
+                        editor.Text = " - Pokušaje očistiti prašinu\n" +
+                            " - Provjerite adapter za punjenje, uvjerite se u njegovu ispravtnost \n" +
+                            " - Provjerite USB kabal";
 
+                        var tapGestureRecognizer = new TapGestureRecognizer();
+                        tapGestureRecognizer.Tapped += (s, ea) =>
+                        {
+                            Device.OpenUri(new Uri("https://www.youtube.com/watch?v=POfGKKxFoas"));
+                        };
+
+                        link.IsVisible = true;
+                        link.GestureRecognizers.Add(tapGestureRecognizer);
+                        link.Text = " - Kliknite ovde za video upustvo čiščenja konektora punjenja";
+                    }
+                    else
+                    if (treci.IsToggled)
+                    {
+                        editor.Text = " - Pokušaje očistiti prašinu\n" +
+                            " - Provjerite adapter za punjenje, uvjerite se u njegovu ispravtnost \n" +
+                            " - Provjerite USB kabal\n" +
+                            " - Provjerite stanje baterije";
+
+                        var tapGestureRecognizer = new TapGestureRecognizer();
+                        tapGestureRecognizer.Tapped += (s, ea) =>
+                        {
+                            Device.OpenUri(new Uri("https://www.youtube.com/watch?v=POfGKKxFoas"));
+                        };
+
+                        link.IsVisible = true;
+                        link.GestureRecognizers.Add(tapGestureRecognizer);
+                        link.Text = " - Kliknite ovde za video upustvo čiščenja konektora punjenja";
+                    }else
+                    if (cetvrti.IsToggled)
+                    {
+                        editor.Text = " - Pokušaje očistiti prašinu\n" +
+                            " - Provjerite adapter za punjenje, uvjerite se u njegovu ispravtnost \n" +
+                            " - Provjerite USB kabal";
+
+                        var tapGestureRecognizer = new TapGestureRecognizer();
+                        tapGestureRecognizer.Tapped += (s, ea) =>
+                        {
+                            Device.OpenUri(new Uri("https://www.youtube.com/watch?v=POfGKKxFoas"));
+                        };
+
+                        link.IsVisible = true;
+                        link.GestureRecognizers.Add(tapGestureRecognizer);
+                        link.Text = " - Kliknite ovde za video upustvo čiščenja konektora punjenja";
+                    }else
+                    if (peti.IsToggled)
+                    {
+                        editor.Text = " - Pokušaje očistiti prašinu\n" +
+                            " - Provjerite adapter za punjenje, uvjerite se u njegovu ispravtnost \n" +
+                             " - Provjerite USB kabal\n" +
+                            " - Provjerite stanje baterije";
+
+                        var tapGestureRecognizer = new TapGestureRecognizer();
+                        tapGestureRecognizer.Tapped += (s, ea) =>
+                        {
+                            Device.OpenUri(new Uri("https://www.youtube.com/watch?v=POfGKKxFoas"));
+                        };
+
+                        link.IsVisible = true;
+                        link.GestureRecognizers.Add(tapGestureRecognizer);
+                        link.Text = " - Kliknite ovde za video upustvo čiščenja konektora punjenja";
+                    }
+
+
+
+                }
+
+                else if (kId == 11) //displey
+                {
+                    if (drugi.IsToggled)
+                    {
+                        editor.Text = " - Potrebno je zamijeniti flat kabal ili cijeli displey, javite se nekom od naših servisa";
+                    }
+                    else if (prvi.IsToggled)
+                    {
+                        editor.Text = " - Provjerite ispravnost i napunjenost baterije\n" +
+                            " - Provjerite ispravnost uređaja, da li se uopšte uključuje\n";
+
+                    }
+                    else
+                     if (treci.IsToggled)
+                    {
+                        editor.Text = " - Potrebno je zamijeniti flat kabal ili cijeli displey, javite se nekom od naših servisa";
+
+                    }
+                    else
+                    if (cetvrti.IsToggled)
+                    {
+                        editor.Text = " - Potrebno je zamijeniti flat kabal ili cijeli displey, javite se nekom od naših servisa";
+
+                    }
+                    else
+                    if (peti.IsToggled)
+                    {
+                        editor.Text = " - Potrebno je zamijeniti flat kabal toucha ili cijeli displey, javite se nekom od naših servisa";
+                    }
+
+
+                }
 
 
 
